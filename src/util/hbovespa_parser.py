57dbeb642e60fc8d1a133030592a9b97b552c7fe
Kevin-Lev <kevinlevrone.r@gmail.com>
2017-10-02 22:17:29 -0300

    Adicionada conversão para Json

--- src/util/hbovespa_parser.py
+++ src/util/hbovespa_parser.py
@@ -1,5 +1,6 @@
 import json
 import os
+import gzip
 
 
 class HBovespaParser(object):
@@ -12,10 +13,39 @@ class HBovespaParser(object):
         """Parses file"""
         print('DECOMPRESSING FILE: %s...' % (self.gzpath))
         # TODO: descompactar o arquivo
+        fileGz = gzip.open(self.gzpath, 'rb')
+        fileContent = open('data/cotacões.txt', 'wb')
+        fileContent.write(fileGz.read())
+        fileGz.close()
+        fileContent.close()
+        fileContent = open('data/cotacões.txt', 'r')
 
+        cotacoes = open("data/cotacoesOrganizadas.json", 'w')
+        listBovespa = [{}]
+
+        for phraseLine in iter(fileContent):
+            flagAux = 0
+            date = ""
+            companyName = ""
+            valor = ""
+            for char in range(2, 230):
+                if(char == 5 or char == 7):
+                    date = date + phraseLine[char] + "/"
+                elif(char <= 9 and (char != 5 or char !=7)):
+                    date = date + phraseLine[char]
+                elif(27 <= char <= 35):
+                    if(phraseLine[char] == " " or flagAux == 1):
+                        if(flagAux == 0):
+                            flagAux = 1
+                        continue
+                    companyName = companyName + phraseLine[char]
+                elif(56 <= char <= 230):
+                    valor = valor + phraseLine[char]
+            listBovespa.append({'Empresa': companyName, 'Data': date, 'Valor': valor})
+        print(listBovespa)
         print('PARSING FILE INTO JSON...')
-        # TODO: converter o arquivo para formato json
-
+        json.dump(listBovespa, cotacoes)
+        cotacoes.close()
         print('OUTPUT FILE WRITTEN TO %s' % (self.outputfile))
 
     def check_json(self):
